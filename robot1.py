9565e8cc8a8ed87dd9e6afc2baf68a5aa5fc846a
zhou robot <minitron@qq.com>
2017-06-02 17:26:50 +0800

    continue
    try qrcode for login

--- robot1.py
+++ robot1.py
@@ -6,7 +6,7 @@ import json
 import time
 import urllib.request, urllib.parse, urllib.error
 import logging
-
+import qrcode
 
 class Robot(object):
 
@@ -21,10 +21,42 @@ class Robot(object):
         self.config = line
         self.appid = 'wx782c26e4c19acffb'
         self.lang = 'zh_CN'
+        self.skey = ''
+        self.uin = ''
+        self.sid = ''
+        self.base_uri = ''
+        self.redirect_uri = ''
 
     def _echo(self, str):
         sys.stdout.write(str)
         sys.stdout.flush()
+
+    def _get(self, url: object, api: object = None, timeout: object = None) -> object:
+        request = urllib.request.Request(url=url)
+        request.add_header('Referer', 'https://wx.qq.com/')
+        if api == 'webwxgetvoice':
+            request.add_header('Range', 'bytes=0-')
+        if api == 'webwxgetvideo':
+            request.add_header('Range', 'bytes=0-')
+        try:
+            response = urllib.request.urlopen(request, timeout=timeout) if timeout else urllib.request.urlopen(request)
+            data = response.read().decode('utf-8')
+            logging.debug(url)
+            return data
+        except urllib.error.HTTPError as e:
+            logging.error('HTTPError = ' + str(e.code))
+        except urllib.error.URLError as e:
+            logging.error('URLError = ' + str(e.reason))
+        except http.client.HTTPException as e:
+            logging.error('HTTPException')
+        except timeout_error as e:
+            pass
+        except ssl.CertificateError as e:
+            pass
+        except Exception:
+            import traceback
+            logging.error('generic exception: ' + traceback.format_exc())
+        return ''
 
     def _post(self, url: object, params: object, jsonfmt: object = True) -> object:
         if jsonfmt:
@@ -84,11 +116,100 @@ class Robot(object):
             logging.debug('[*] 退出程序')
             exit()
 
+    def genQRCode(self):
+        #return self._showQRCodeImg()
+        '''
+        if sys.platform.startswith('win'):
+            self._showQRCodeImg('win')
+        elif sys.platform.find('darwin') >= 0:
+            self._showQRCodeImg('macos')
+        else:
+        '''
+        self._str2qr('https://login.weixin.qq.com/l/' + self.uuid)
+
+    def _str2qr(self, str):
+        print(str)
+        qr = qrcode.QRCode()
+        qr.border = 1
+        qr.add_data(str)
+        qr.make()
+        # img = qr.make_image()
+        # img.save("qrcode.png")
+        #mat = qr.get_matrix()
+        #self._printQR(mat)  # qr.print_tty() or qr.print_ascii()
+        qr.print_ascii(invert=True)
+
+    def waitForLogin(self, tip=1):
+        time.sleep(tip)
+        url = 'https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login?tip=%s&uuid=%s&_=%s' % (
+            tip, self.uuid, int(time.time()))
+        data = self._get(url)
+        if data == '':
+            return False
+        pm = re.search(r"window.code=(\d+);", data)
+        code = pm.group(1)
+
+        self._echo(code)
+        if code == '201':
+            return True
+        elif code == '200':
+            pm = re.search(r'window.redirect_uri="(\S+?)";', data)
+            r_uri = pm.group(1) + '&fun=new'
+            self.redirect_uri = r_uri
+            self.base_uri = r_uri[:r_uri.rfind('/')]
+            return True
+        elif code == '408':
+            self._echo('[登陆超时] \n')
+        else:
+            self._echo('[登陆异常] \n')
+        return False
+
+    def login(self):
+        data = self._get(self.redirect_uri)
+        if data == '':
+            return False
+        doc = xml.dom.minidom.parseString(data)
+        root = doc.documentElement
+
+        for node in root.childNodes:
+            if node.nodeName == 'skey':
+                self.skey = node.childNodes[0].data
+            elif node.nodeName == 'wxsid':
+                self.sid = node.childNodes[0].data
+            elif node.nodeName == 'wxuin':
+                self.uin = node.childNodes[0].data
+            elif node.nodeName == 'pass_ticket':
+                self.pass_ticket = node.childNodes[0].data
+
+        if '' in (self.skey, self.sid, self.uin, self.pass_ticket):
+            return False
+
+        self.BaseRequest = {
+            'Uin': int(self.uin),
+            'Sid': self.sid,
+            'Skey': self.skey,
+            'DeviceID': self.deviceId,
+        }
+        return True
+
+
+
     def start(self):
         # connect wx server
         self._echo('[*] 微信网页版 ... 开动\n')
-        self._run('[*] 正在获取 uuid ... ', self.getUUID)
-        self._echo('[*] 正在获取二维码 ... 成功')
+        while True:
+            self._run('[*] 正在获取 uuid ... ', self.getUUID)
+            self._echo('[*] 正在获取二维码 ... 成功')
+            self.genQRCode()
+            self._echo('[*] 请使用微信扫描二维码以登录 ... ')
+            if not self.waitForLogin():
+                continue
+                print('[*] 请在手机上点击确认以登录 ... ')
+            if not self.waitForLogin(0):
+                continue
+            break
+
+        self._run('[*] 正在登录 ... ', self.login)
 
 if __name__ == '__main__':
     Robot1 = Robot()
